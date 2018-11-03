abd85a390212a0f77cc58dd3607f2beec061f860
Sani Elfishawy <elfishawy.sani@gmail.com>
2018-11-02 17:14:39 -0700

    TabularData widget created and tested new file:   tests/test_tabular_data.py new file:   tk_canvas_renderers/tabular_data.py

---
+++ tests/test_tabular_data.py
@@ -0,0 +1,48 @@
+# pylint: disable=C0413
+import sys
+import os
+import unittest
+
+sys.path.insert(0, os.getcwd())
+from tk_canvas_renderers.tabular_data import TabularData
+
+class TestTabularData(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test(self):
+        data = [
+            {
+                TabularData.LABEL: 'col0 row0',
+                TabularData.VALUE: 0,
+            },
+            {
+                TabularData.LABEL: 'col0 row1',
+                TabularData.VALUE: 1,
+            },
+            {
+                TabularData.LABEL: 'col2 row0',
+                TabularData.COLUMN: 2,
+                TabularData.VALUE: 2,
+            },
+            {
+                TabularData.LABEL: 'col1 row0',
+                TabularData.COLUMN: 1,
+                TabularData.VALUE: 3,
+            },
+            {
+                TabularData.LABEL: 'col0 row3',
+                TabularData.VALUE: 4,
+            },
+            {
+                TabularData.LABEL: 'col0 row4',
+                TabularData.VALUE: 5,
+            },
+        ]
+        tabular_data = TabularData(data=data)
+        tabular_data.run()
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tk_canvas_renderers/tabular_data.py
@@ -0,0 +1,95 @@
+import tkinter as tk
+
+class TabularData:
+    '''
+    Takes data as a list of dicts
+    [
+        {
+            label: required,
+            value: optional,
+            column: optional,
+        },
+        ...
+    ]
+    '''
+
+    LABEL = 'label'
+    VALUE = 'value'
+    COLUMN = 'column'
+
+    def __init__(
+            self,
+            data: dict,
+    ):
+        self._data = data
+        self._column_frames = None
+        self._value_fields = {}
+
+    def setup_ui(self, master):
+        self._column_frames = [
+            tk.Frame(master=master)
+            for _ in list(range(self._get_num_columns()))
+        ]
+
+        for column, column_frame in enumerate(self._column_frames):
+            column_frame.grid(
+                column=column,
+                row=0,
+                sticky=tk.NE,
+            )
+
+        for column in range(self._get_num_columns()):
+            for row, label in enumerate(self._get_labels_in_column(column)):
+                tk.Label(
+                    self._column_frames[column],
+                    text=label,
+                ).grid(
+                    column=0,
+                    row=row,
+                    sticky=tk.E,
+                )
+                self._value_fields[label] = tk.Label(
+                    self._column_frames[column],
+                )
+                self._value_fields[label].grid(
+                    column=1,
+                    row=row,
+                    sticky=tk.E,
+                )
+        self._update_values(data=self._data)
+
+    def _get_column_from_entry(self, entry: dict) -> int:
+        if self.__class__.COLUMN in entry:
+            return entry[self.__class__.COLUMN]
+        return 0
+
+    def _get_value_with_label(self, label: str, data: list) -> str:
+        for entry in data:
+            if entry[self.__class__.LABEL] == label and self.__class__.VALUE in entry:
+                return str(entry[self.__class__.VALUE])
+        return ''
+
+    def _get_num_columns(self):
+        return max([self._get_column_from_entry(entry) for entry in self._data]) + 1
+
+    def _get_labels_in_column(self, column):
+        r = []
+        for entry in self._data:
+            if self._get_column_from_entry(entry) == column:
+                r.append(entry[self.__class__.LABEL])
+        return r
+
+    def _update_values(self, data):
+        for entry in data:
+            label = entry[self.__class__.LABEL]
+            self._value_fields[label].config(
+                text=self._get_value_with_label(
+                    label=label,
+                    data=data,
+                )
+            )
+
+    def run(self):
+        master = tk.Tk()
+        self.setup_ui(master)
+        master.mainloop()
